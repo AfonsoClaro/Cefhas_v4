fc291742003181958a519cfbef9b720bf7276ee7
João Godinho <aluno21883@ipt.pt>
2023-05-16 18:06:36 +0100

    Home page

--- App.js
+++ App.js
@@ -1,34 +1,44 @@
 import * as React from 'react';
-import { Button, View, Image, StyleSheet, Text, Alert } from 'react-native';
+import { Button, View, Image, StyleSheet, Text, Alert, ImageBackground } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 import * as AzureAuth from 'react-native-azure-auth';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center',
   },
   titulo:{
     fontFamily:'',
     fontSize:75,
-    color:'dimgray',
+    fontWeight:'bold',
+    color:'white',
     position:'absolute',
     top:50,
-
+    textShadowRadius:15,
+    textShadowColor:'black',
+  },
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   logo1: {
     width: 150,
     height: 70,
     position:'absolute',
     bottom:15,
-    left:15,
+    right:75,
   },
   logo2: {
     width: 150,
     height: 70,
     position:'absolute',
     bottom:15,
-    right:15,
+    left:75,
   },
 });
 
@@ -54,24 +64,30 @@ function HomeScreen({ navigation }) {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.titulo}
-      >CEFHAS</Text>
-      <Image
-        style={styles.logo1}
-        source={require('./assets/ipt.png')}
-      />
-      <Image
-        style={styles.logo2}
-        source={require('./assets/LabCR.jpg')}
-      />
-      <Button 
-        //onPress={handleLogin}
-        onPress={() => Alert.alert('Login não implementado')} 
-        title="Microsoft Login" 
-        color="gray"
-      />
-    </View>
+    <ImageBackground
+      source={require('./assets/fundo.jpg')}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text style={styles.titulo}
+        >CEFHAS</Text>
+        <Image
+          style={styles.logo1}
+          source={require('./assets/ipt.png')}
+        />
+        <Image
+          style={styles.logo2}
+          source={require('./assets/LabCR.jpg')}
+        />
+        <Button 
+          //onPress={handleLogin}
+          onPress={() => Alert.alert('Login não implementado')} 
+          title="Microsoft Login" 
+          color="gray"
+          
+        />
+      </View>
+    </ImageBackground>
   );
 }
 
@@ -106,7 +122,7 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator initialRouteName="Home" >
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Galeria" component={Galeria} />
         <Drawer.Screen name="Fichas de Inventário" component={FichaInventario} />
